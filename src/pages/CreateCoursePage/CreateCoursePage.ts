bcff56571dc5c7bd89bc0d6c44bb708eb5b8c9e4
GuillaumeBAECHLER <baechler.guillaume.pro@gmail.com>
2016-10-06 14:17:16 +0200

    modifs page
    motifs pages

---
+++ src/pages/CreateCoursePage/CreateCoursePage.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'CreateCoursePage',
+  templateUrl: 'CreateCoursePage.html'
+})
+export class CreateCoursePage {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+}
